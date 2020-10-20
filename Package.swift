9a0f52c6ae860f726541dfcbcf881cf1a5c55218
Arnaud Joubay <jub@sowenga.net>
2020-10-20 13:28:54 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/pvieito/PythonKit.git", .branch("master"))
+        .package(url: "https://github.com/pvieito/PythonKit.git", .revision("a0817e0d68f0a50a631fc5fab0b8d219fd010241"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
